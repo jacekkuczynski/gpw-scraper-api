3298c8105bc88e901974a57d9efe6c41100ee0db
Jacek Kuczyński <86817239+jacekkuczynski@users.noreply.github.com>
2023-03-23 11:10:17 +0100

    backend/ install middlewares
    install morgan
    install helmet

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import morgan from "morgan";
 import { scrapeAllCompaniesInitialData } from "./scrapeFunctions/company/scrapeAllCompaniesInitialData/scrapeAllCompaniesInitialData";
 import { readAllCompaniesData, readCompanyProfile } from "./prisma";
 
 export const prisma = new PrismaClient();
 
+//  helmet with ES6 export syntax breaks the app for now
+const helmet = require("helmet");
+
 const server = async () => {
   await scrapeAllCompaniesInitialData();
 
   const app = express();
-  const port = 3001;
+  app.use(helmet());
+  app.use(morgan("tiny")); //  log format ":method :url :status :res[content-length] - :response-time ms"
 
-  app.get("/data", async (req, res) => {
+  app.get("/all-companies-data", async (req, res) => {
     const allCompaniesData = await readAllCompaniesData(prisma);
     res.json(allCompaniesData);
   });
@@ -22,6 +27,7 @@ const server = async () => {
     res.json(companyProfile);
   });
 
+  const port = process.env.PORT;
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
