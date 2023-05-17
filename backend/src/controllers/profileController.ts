2823352b8f006a70b70fca9d37cc95b8c14db952
Jacek Kuczyński <86817239+jacekkuczynski@users.noreply.github.com>
2023-05-17 14:14:46 +0200

    Update profileController.ts

--- backend/src/controllers/profileController.ts
+++ backend/src/controllers/profileController.ts
@@ -2,14 +2,10 @@ import { prisma } from "..";
 import { readCompanyProfile } from "../prisma";
 
 export const getCompanyProfile = async (req, res) => {
-  const everySymbolLength = 3;
-
   try {
     const symbol = req.query.symbol.toString().toUpperCase();
-    if (symbol.length === everySymbolLength) {
-      const companyProfile = await readCompanyProfile(prisma, symbol);
-      res.status(200).json(companyProfile);
-    } else res.status(418).json({ error: "Invalid symbol" });
+    const companyProfile = await readCompanyProfile(prisma, symbol);
+    res.status(200).json(companyProfile);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
