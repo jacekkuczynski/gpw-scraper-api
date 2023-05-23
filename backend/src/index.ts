b980ee6bd6abf0241b946353f3de0c843078e116
Jacek Kuczyński <86817239+jacekkuczynski@users.noreply.github.com>
2023-05-23 20:31:47 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,13 +6,11 @@ import cors from "cors";
 import helmet from "helmet";
 import { scheduleDailyPricesScrape } from "./schedule/scheduleDailyPricesScrap";
 import router from "./routes";
-import { scrapeAllCompaniesDailyPrice } from "./scrapeFunctions/price/scrapeCompanyPriceData/scrapeAllCompaniesDailyPrice/scrapeAllCompaniesDailyPrice";
 
 export const prisma = new PrismaClient();
 const port = process.env.PORT;
 
 const server = async () => {
-  await scrapeAllCompaniesDailyPrice();
   await scrapeAllCompaniesInitialData();
   scheduleDailyPricesScrape();
 
