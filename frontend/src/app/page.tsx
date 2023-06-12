949640b5b67ed68006ef2a7574cb310a6d0c5f7e
Jacek Kuczyński <idea.jacek@gmail.com>
2023-06-13 01:43:47 +0200

    landing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,38 +7,36 @@ export default function Home() {
   return (
     <div className={styles.landing}>
       <div className={`${styles.version} ${robotoMono.className}`}>
-        <b>Virtual Wallet 3</b>
+        <h1>Virtual Wallet 3</h1>
         <span>{version}</span>
-        <br />
       </div>
-      <div>
-        Full-stack application written in TypeScript.
-        <br />
-        All of the data visible on the page is automatically scraped.
-        <br />
-        For price updates scraper runs everyday after the market closes.
-        <br />
-        <br />
-        <ul className={styles.techList}>
-          <b>Server:</b>
+      <div className={styles.appDescription}>
+        <p>Full-stack application written in TypeScript.</p>
+        <p>All of the data visible on the page is automatically scraped.</p>
+        <p>
+          For price updates scraper runs on weekdays after the market closes.
+        </p>
+        <p>User data is stored in the browser.</p>
+      </div>
+      <div className={styles.techList}>
+        <b>Server:</b>
+        <ul>
           <li>Node.js</li>
           <li>Express.js</li>
           <li>Puppeteer.js</li>
-          <br />
-          <b>Database:</b>
+        </ul>
+        <b>Database:</b>
+        <ul>
           <li>MySQL</li>
           <li>Prisma</li>
-          <br />
-          <b>Frontend:</b>
-          <ul>
-            <ul>
-              <li>React with Next.js</li>
-              <li>Radix-ui with css.modules</li>
-              <li>Axios with React Query</li>
-              <li>Zustand</li>
-              <li>Lightweight Charts</li>
-            </ul>
-          </ul>
+        </ul>
+        <b>Frontend:</b>
+        <ul>
+          <li>React with Next.js</li>
+          <li>Radix-ui with css.modules</li>
+          <li>Axios with React Query</li>
+          <li>Zustand</li>
+          <li>Lightweight Charts</li>
         </ul>
       </div>
     </div>
