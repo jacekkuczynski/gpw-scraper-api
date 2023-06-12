b295652481e52e7a5aabbe74f04a2a42387f285b
Jacek Kuczyński <idea.jacek@gmail.com>
2023-06-13 01:25:23 +0200

    landing page

--- frontend/src/app/fonts.ts
+++ frontend/src/app/fonts.ts
@@ -1,10 +1,6 @@
-import { Inter, Monofett, Space_Mono } from "next/font/google";
+import { Inter, Monofett, Roboto_Mono, Space_Mono } from "next/font/google";
 
-export const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
-
+export const inter = Inter({ subsets: ["latin"] });
 export const monofett = Monofett({ weight: "400", subsets: ["latin"] });
-
 export const spaceMono = Space_Mono({ weight: "400", subsets: ["latin"] });
+export const robotoMono = Roboto_Mono({ weight: "400", subsets: ["latin"] });
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,46 @@
+import { robotoMono } from "./fonts";
+import styles from "./page.module.css";
+
+const version = "1.0 beta";
+
 export default function Home() {
-  return <div style={{ width: "100%" }}>Hello world</div>;
+  return (
+    <div className={styles.landing}>
+      <div className={`${styles.version} ${robotoMono.className}`}>
+        <b>Virtual Wallet 3</b>
+        <span>{version}</span>
+        <br />
+      </div>
+      <div>
+        Full-stack application written in TypeScript.
+        <br />
+        All of the data visible on the page is automatically scraped.
+        <br />
+        For price updates scraper runs everyday after the market closes.
+        <br />
+        <br />
+        <ul className={styles.techList}>
+          <b>Server:</b>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>Puppeteer.js</li>
+          <br />
+          <b>Database:</b>
+          <li>MySQL</li>
+          <li>Prisma</li>
+          <br />
+          <b>Frontend:</b>
+          <ul>
+            <ul>
+              <li>React with Next.js</li>
+              <li>Radix-ui with css.modules</li>
+              <li>Axios with React Query</li>
+              <li>Zustand</li>
+              <li>Lightweight Charts</li>
+            </ul>
+          </ul>
+        </ul>
+      </div>
+    </div>
+  );
 }
